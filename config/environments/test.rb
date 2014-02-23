c70a08548bd0e99e794ff2589a2740d3f3f7b995
quality-leaf <oh.ayaka.ballpen@gmail.com>
2014-02-23 23:41:26 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Improve the test speed by losing cost of bcrypt
+  ActiveModel::SecurePassword.min_cost = true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,15 +44,6 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it{ should_not be_valid }
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
@@ -90,6 +81,16 @@ describe User do
 
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
 end 
